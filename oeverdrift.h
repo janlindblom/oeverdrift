146d5309085aae3972a7cebf8e44f92475c4a2dc
Jan Lindblom <janlindblom@fastmail.fm>
2022-11-11 13:33:12 +0200

    Some cleaning up and trying to get a better state for on/off.

--- oeverdrift.h
+++ oeverdrift.h
@@ -5,19 +5,26 @@
 
 #pragma once
 
+#include <AceButton.h>
+using namespace ace_button;
+
 #define LED_PIN 0
 #define NUM_LEDS 16
-#define LED_ADDR 0
 
 #define KEY_LEVEL 3
 #define KEY_ONOFF 4
 #define KEY_MODE 5
 
 #define LIGHT_LEVELS 6
-#define LIGHT_ADDR LED_ADDR + (NUM_LEDS * 2)
+#define MODES 3
+#define LIGHT_ADDR 0
+#define MODE_ADDR LIGHT_ADDR + 1
+#define LED_ADDR MODE_ADDR + 1
 
 void set_brightness(uint8_t brightness);
-void decrease_brightness();
 void increase_brightness();
 void key_actions();
 void keyboard_handler();
+void handleEvent(AceButton *button, uint8_t eventType, uint8_t buttonState);
+void rotate_mode();
+void set_mode();
--- oeverdrift.ino
+++ oeverdrift.ino
@@ -3,154 +3,217 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-#include "FIFO.h"
 #include "oeverdrift.h"
+#include <AceButton.h>
 #include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
 
-uint32_t warmer_white = 0xFF7847;
-uint32_t warm_white = 0xFF785A;
-uint32_t white = 0xFFFFFF;
+using namespace ace_button;
 
-enum modes { WHITE, WARM, HOT };
+uint32_t warmer_white = 0xFF501F;
+uint32_t warm_white = 0xFF6433;
+uint32_t white = 0xFF7847;
+uint32_t black = 0;
 
-enum mode_style { STATIC, DYNAMIC };
+enum modes { OFF,
+             WHITE,
+             WARM,
+             WARMER };
 
-uint8_t mode = WHITE;
+enum mode_style { STATIC,
+                  DYNAMIC };
 
-uint32_t pattern_white[] = {0xFF7847, 0xFF7847, 0xFF7847, 0xFF7847,
-                            0xFF7847, 0xFF7847, 0xFF7847, 0xFF7847,
-                            0xFF7847, 0xFF7847, 0xFF7847, 0xFF7847,
-                            0xFF7847, 0xFF7847, 0xFF7847, 0xFF7847};
-uint32_t pattern_warmwhite[] = {0xFF6433, 0xFF6433, 0xFF6433, 0xFF6433,
-                                0xFF6433, 0xFF6433, 0xFF6433, 0xFF6433,
-                                0xFF6433, 0xFF6433, 0xFF6433, 0xFF6433,
-                                0xFF6433, 0xFF6433, 0xFF6433, 0xFF6433};
-uint32_t pattern_hotwhite[] = {0xFF501F, 0xFF501F, 0xFF501F, 0xFF501F,
-                               0xFF501F, 0xFF501F, 0xFF501F, 0xFF501F,
-                               0xFF501F, 0xFF501F, 0xFF501F, 0xFF501F,
-                               0xFF501F, 0xFF501F, 0xFF501F, 0xFF501F};
+uint32_t pattern_off[NUM_LEDS] = { black, black, black, black,
+                                   black, black, black, black,
+                                   black, black, black, black,
+                                   black, black, black, black };
+uint32_t pattern_white[NUM_LEDS] = { white, white, white, white,
+                                     white, white, white, white,
+                                     white, white, white, white,
+                                     white, white, white, white };
+uint32_t pattern_warmwhite[NUM_LEDS] = { warm_white, warm_white, warm_white, warm_white,
+                                         warm_white, warm_white, warm_white, warm_white,
+                                         warm_white, warm_white, warm_white, warm_white,
+                                         warm_white, warm_white, warm_white, warm_white };
+uint32_t pattern_warmerwhite[NUM_LEDS] = { warmer_white, warmer_white, warmer_white, warmer_white,
+                                           warmer_white, warmer_white, warmer_white, warmer_white,
+                                           warmer_white, warmer_white, warmer_white, warmer_white,
+                                           warmer_white, warmer_white, warmer_white, warmer_white };
 
 // uint32_t (*)[] modes[] = {, &pattern_warmwhite, &pattern_hotwhite};
-uint8_t mode_styles[] = {STATIC, STATIC, STATIC};
+uint8_t mode_styles[] = { STATIC, STATIC, STATIC };
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB);
 
-uint8_t sine[] = {4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5};
-uint32_t led_state[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t sine[NUM_LEDS] = { 4, 3, 2, 1, 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5 };
+uint32_t led_state[NUM_LEDS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-uint16_t hue = 0;
-uint8_t light_level[] = {0x00, 0x33, 0x66, 0x99, 0xCC, 0xFF};
+uint8_t light_level[6] = { 0x00, 0x33, 0x66, 0x99, 0xCC, 0xFF };
 
-uint8_t brightness_level = 1;
-uint8_t col = 0;
+volatile uint8_t brightness_level = 3;
+volatile uint8_t current_mode = WARM;
+volatile bool lamp_is_on = true;
 
-// Our keymap
-uint8_t key_map[] = {KEY_LEVEL, KEY_ONOFF, KEY_MODE};
-// Initial states of keys (HIGH since inputs are pulled up)
-uint8_t key_state_previous[] = {HIGH, HIGH, HIGH};
-FIFO key_buf;
+AceButton level_button;
+AceButton onoff_button;
+AceButton mode_button;
 
 void paint(uint32_t pattern[]) {
   for (uint8_t i = 0; i < sizeof(led_state) / sizeof(led_state[0]); i++) {
     strip.setPixelColor(i, pattern[i]);
     led_state[i] = pattern[i];
   }
-
   strip.show();
 }
 
 void setup() {
   // Setup the ÖVERDRIFT
-  EEPROM.begin(512);
-
-  for (uint8_t key = 0; key < sizeof(key_map) / sizeof(key_map[0]); key++) {
-    pinMode(key_map[key], PinMode::INPUT_PULLUP);
-  }
-  delay(100);
-
-  strip.begin();
-  // for (uint8_t i = 0; i < NUM_LEDS; i++) {
-
-  // strip.setPixelColor(i, wew);
-  //    EEPROM.write(LED_ADDR + i, hue >> 8);
-  //    EEPROM.write(LED_ADDR + i * 2, hue & 0xFF);
-  // }
-  paint(pattern_hotwhite);
-
-  brightness_level = EEPROM.read(LIGHT_ADDR);
-  strip.setBrightness(light_level[brightness_level]);
-
-  // Keyboard.begin();
+  // Start a Serial
   if (!Serial) {
     Serial.begin(9600);
   }
+  delay(100);
+
+  EEPROM.begin(2 + (NUM_LEDS * 2));
+
+  pinMode(KEY_LEVEL, INPUT_PULLUP);
+  pinMode(KEY_ONOFF, INPUT_PULLUP);
+  pinMode(KEY_MODE, INPUT_PULLUP);
+  level_button.init(KEY_LEVEL);
+  onoff_button.init(KEY_ONOFF);
+  mode_button.init(KEY_MODE);
+
+  ButtonConfig* buttonConfig = ButtonConfig::getSystemButtonConfig();
+  buttonConfig->setEventHandler(handleEvent);
+  buttonConfig->setFeature(ButtonConfig::kFeatureClick);
+  buttonConfig->setFeature(ButtonConfig::kFeatureDoubleClick);
+
+  // Start the light strip
+  strip.begin();
+
+  current_mode = EEPROM.read(MODE_ADDR);
+  if (current_mode == OFF) {
+    current_mode = WARM;
+  }
+  if (Serial) {
+    Serial.print("Mode: ");
+    Serial.println(current_mode);
+  }
+  set_mode();
+
+  // Set brightness level
+  brightness_level = EEPROM.read(LIGHT_ADDR);
+  if (Serial) {
+    Serial.print("Brightness: ");
+    Serial.println(brightness_level);
+  }
+  strip.setBrightness(light_level[brightness_level]);
+  strip.show();
 }
 
 void loop() {
-  keyboard_handler();
-  key_actions();
+  // Button scanner loop
+  onoff_button.check();
+  level_button.check();
+  mode_button.check();
 }
 
-void key_actions() {
-  while (key_buf.size() > 0) {
-    uint8_t key = key_buf.pop();
-    if (key == KEY_LEVEL) {
-      increase_brightness();
-    }
-    if (key == KEY_ONOFF) {
-      if (brightness_level != 0) {
-        set_brightness(0);
-      } else {
-        set_brightness(brightness_level);
-      }
-    }
-    if (key == KEY_MODE) {
-    }
+void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+  if (!Serial) {
+    Serial.begin(9600);
   }
-}
-
-/**
-   Keyboard scanner routine.
-*/
-void keyboard_handler() {
-  for (uint8_t key = 0; key < sizeof(key_map) / sizeof(key_map[0]); key++) {
-    uint8_t state = digitalRead(key_map[key]);
-
-    if ((state != key_state_previous[key]) && (state != HIGH)) {
-      key_buf.push(key_map[key]);
+  switch (eventType) {
+    case AceButton::kEventClicked:
       if (Serial) {
-        Serial.print("Key ");
-        Serial.print(key_map[key], DEC);
-        Serial.print(" state: ");
-        Serial.println(state, DEC);
-        Serial.println("Registering keypress.");
+        Serial.print("Button: ");
+        Serial.print(button->getPin());
+        Serial.print(", event: ");
+        Serial.print(eventType);
+        Serial.print(", buttonState: ");
+        Serial.println(buttonState);
       }
-    }
-    key_state_previous[key] = state;
+      if (button->getPin() == KEY_LEVEL) {
+        increase_brightness();
+      } else if (button->getPin() == KEY_ONOFF) {
+        if (Serial) {
+          Serial.printf("Lamp on? %b\n", lamp_is_on);
+        }
+        if (lamp_is_on) {
+          paint(pattern_off);
+          set_brightness(0);
+          strip.show();
+        } else {
+          set_mode();
+          set_brightness(brightness_level);
+          strip.show();
+        }
+        lamp_is_on = !lamp_is_on;
+      } else if (button->getPin() == KEY_MODE) {
+        rotate_mode();
+      }
+      break;
+    case AceButton::kEventDoubleClicked:
+      if (Serial) {
+        Serial.printf("Double click: %d, buttonState: %d\n", button->getPin(), buttonState);
+      }
   }
 }
 
 void set_brightness(uint8_t brightness) {
-  EEPROM.write(LIGHT_ADDR, brightness);
-  strip.setBrightness(light_level[brightness]);
-  EEPROM.commit();
-}
-
-void decrease_brightness() {
-  if (brightness_level < 1) {
-    brightness_level = 0;
-  } else {
-    brightness_level--;
+  if (Serial) {
+    Serial.print("Setting brightness: ");
+    Serial.println(brightness);
   }
-  set_brightness(brightness_level);
+  strip.setBrightness(light_level[brightness]);
+  strip.show();
+  if (brightness > 0) {
+    EEPROM.write(LIGHT_ADDR, brightness);
+    EEPROM.commit();
+  }
 }
 
 void increase_brightness() {
-  if (brightness_level > 3) {
+  if (!lamp_is_on) {
+    return;
+  }
+  if (brightness_level > 4) {
     brightness_level = 1;
   } else {
     brightness_level++;
   }
   set_brightness(brightness_level);
+}
+
+void set_mode() {
+  if (!lamp_is_on) {
+    return;
+  }
+  if (Serial) {
+    Serial.print("Setting mode: ");
+    Serial.println(current_mode);
+  }
+  switch (current_mode) {
+    case WHITE:
+      paint(pattern_white);
+      break;
+    case WARM:
+      paint(pattern_warmwhite);
+      break;
+    case WARMER:
+      paint(pattern_warmerwhite);
+      break;
+  }
+}
+
+void rotate_mode() {
+  if (!lamp_is_on) {
+    return;
+  }
+  current_mode++;
+  if (current_mode >= WARMER) {
+    current_mode = WHITE;
+  }
+  set_mode();
+  EEPROM.write(MODE_ADDR, current_mode);
+  EEPROM.commit();
 }
